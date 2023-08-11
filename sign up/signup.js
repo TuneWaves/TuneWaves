64c2b4f9d074542989f5eee434d21f4c8e8e4d42
FediMaalej <fm3aalej@gmail.com>
2023-08-11 13:25:49 +0100

    signup

--- sign up/signup.js
+++ sign up/signup.js
@@ -5,31 +5,47 @@ userName: $('#user_name').val(),
 password : $('#password').val(),
 }
 arr.push(dataUser)
-var setitem=localStorage.setItem('Sign_Ups',JSON.stringify(arr))
+localStorage.setItem('Sign_Ups',JSON.stringify(arr))
+
 location.replace("../index2.html")
 })
 //login
 
-/* var arrLog = JSON.parse(localStorage.getItem("setitem"))
+var arrLog = JSON.parse(localStorage.getItem('Sign_Ups'))
 var user = $('.user_log').val()
 var pass = $('.pass_log').val()
 
 $('#log_btn').click(function(){
-  var filtered= find(arrLog,function(e){
-  if(!filtered.length){
-    $('.msg').append(`
-    <p class="Items">You don't have an acount</p>
-   `)
-  }
-  else if(e.userName === user && e.email === mail && e.password === pass ){
-    return location.replace("../index2/index2.html")
-  }
- else{
-  $('.msg').append(`<div class="slide">
+  var user = $('.user_log').val()
+var pass = $('.pass_log').val()
+  each(arrLog,function(e){
+    console.log("user",user);
+  console.log("eeee",e.userName);
+   if(!user.length || !pass.length){
+    $('.msg').append(`<div class="slide">
   <p class="Items">You don't have an acount</p>
-  </div>
-  </div>`)
+   </div>`)
+   }
+  else if(user == e.userName && pass == e.password){
+  console.log("nice")
+     location.replace("../index2.html")
+  }
+  else{
+    $('.msg').append(`<div class="slide">
+    <p class="Items">You should verrif your password</p>
+     </div>`)
+ 
   }
 })
 })
- */
+function each(coll, f) { 
+  if (Array.isArray(coll)) { 
+        for (var i = 0; i < coll.length; i++) { 
+              f(coll[i], i); 
+        } 
+  } else { 
+        for (var key in coll) { 
+              f(coll[key], key); 
+        } 
+  } 
+}
